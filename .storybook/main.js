622e64acbcc67857fd7f26b42ea575ad66f9f5eb
byn9826 <byn9826@gmail.com>
2020-03-16 23:13:32 -0400

    Remove storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
 module.exports = {
   stories: ['../stories/**/*.stories.jsx'],
-  addons: ['@storybook/addon-storysource'],
   webpackFinal: async config => {
     // do mutation to the config
     return config;
