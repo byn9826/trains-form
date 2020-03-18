c2d40168d5ecab9620004d2ba6495f18c1fe1d34
byn9826 <byn9826@gmail.com>
2020-03-18 13:43:37 -0400

    Bootstrap radio type

--- src/elements/Radio.jsx
+++ src/elements/Radio.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as Types from '../helpers/types';
-import { SEMANTIC_THEME } from '../helpers/constants';
+import { getRadioClasses } from '../helpers/theme';
 
 export default function Radio({
   disabled,
@@ -12,45 +12,46 @@ export default function Radio({
   onChange,
 }) {
   const onClick = (option) => onChange(name, option.value);
+  const classNames = getRadioClasses(theme);
 
-  switch (theme) {
-    case SEMANTIC_THEME:
-    default:
-      return (
-        <div
-          className="inline fields"
-          style={{
-            ...style,
-            flexWrap: 'wrap',
-          }}
-        >
-          {
-            options.map((option) => (
-              <div
-                key={option.value}
-                className="field"
-                onClick={() => onClick(option)}
-                onKeyDown={() => onClick(option)}
-                role="button"
-              >
-                <div className="ui radio checkbox">
-                  <input
-                    type="radio"
-                    name={name}
-                    checked={value === option.value}
-                    className="hidden"
-                    readOnly
-                    tabIndex="0"
-                    disabled={disabled}
-                  />
-                  <label>{option.label}</label>
-                </div>
-              </div>
-            ))
-          }
-        </div>
-      );
-  }
+  return (
+    <div
+      className={classNames.radioGroup}
+      style={{
+        ...style,
+        flexWrap: 'wrap',
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
+      {
+        options.map((option) => (
+          <div
+            key={option.value}
+            className={classNames.radioContainer}
+            onClick={() => onClick(option)}
+            onKeyDown={() => onClick(option)}
+            role="button"
+          >
+            <div className={classNames.radio}>
+              <input
+                type="radio"
+                name={name}
+                checked={value === option.value}
+                readOnly
+                tabIndex="0"
+                disabled={disabled}
+                className={classNames.radioInput}
+              />
+              <label className={classNames.radioLabel}>
+                {option.label}
+              </label>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  );
 }
 
 Radio.propTypes = Types.ELEMENT_TYPE;
